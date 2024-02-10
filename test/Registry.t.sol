88d56a43d9a94d008daafde9a0a96204679891b3
anish-ritual <anish@ritual.net>
2024-02-10 03:55:29 -0500

    feat: update Registry tests

--- test/Registry.t.sol
+++ test/Registry.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.4;
 
+import {Inbox} from "../src/Inbox.sol";
 import {Test} from "forge-std/Test.sol";
 import {Registry} from "../src/Registry.sol";
 import {LibDeploy} from "./lib/LibDeploy.sol";
@@ -23,6 +24,9 @@ contract RegistryTest is Test {
     /// @notice Coordinator
     EIP712Coordinator internal COORDINATOR;
 
+    /// @notice Inbox
+    Inbox internal INBOX;
+
     /*//////////////////////////////////////////////////////////////
                                  SETUP
     //////////////////////////////////////////////////////////////*/
@@ -32,13 +36,15 @@ contract RegistryTest is Test {
         uint256 initialNonce = vm.getNonce(address(this));
         address nodeManagerAddress = vm.computeCreateAddress(address(this), initialNonce + 1);
         address coordinatorAddress = vm.computeCreateAddress(address(this), initialNonce + 2);
+        address inboxAddress = vm.computeCreateAddress(address(this), initialNonce + 3);
 
         // Deploy registry
-        REGISTRY = new Registry(nodeManagerAddress, coordinatorAddress);
+        REGISTRY = new Registry(nodeManagerAddress, coordinatorAddress, inboxAddress);
 
-        // Deploy node manager + coordinator
+        // Deploy node manager, coordinator, inbox
         NODE_MANAGER = new NodeManager();
         COORDINATOR = new EIP712Coordinator(REGISTRY);
+        INBOX = new Inbox(REGISTRY);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -49,18 +55,20 @@ contract RegistryTest is Test {
     function testRegistryAddresses() public {
         assertEq(REGISTRY.NODE_MANAGER(), address(NODE_MANAGER));
         assertEq(REGISTRY.COORDINATOR(), address(COORDINATOR));
+        assertEq(REGISTRY.INBOX(), address(INBOX));
     }
 
     /// @notice Check registry addresses correctly correspond to deployed counterparts when using LibDeploy
     function testRegistryViaLibDeploy() public {
         // Deploy via LibDeploy
         uint256 initialNonce = vm.getNonce(address(this));
-        (Registry registry, NodeManager nodeManager, EIP712Coordinator coordinator) =
+        (Registry registry, NodeManager nodeManager, EIP712Coordinator coordinator, Inbox inbox) =
             LibDeploy.deployContracts(initialNonce);
 
         // Assert checks
         // Note: these are somewhat redundant given LibDeploy also `require`-checks at deploy time, but useful for future safety
         assertEq(registry.NODE_MANAGER(), address(nodeManager));
         assertEq(registry.COORDINATOR(), address(coordinator));
+        assertEq(registry.INBOX(), address(inbox));
     }
 }
