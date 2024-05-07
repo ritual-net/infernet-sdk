667f8e50dbc03438e62d68cfcab07ae33716da93
anish-ritual <anish@ritual.net>
2024-05-07 00:21:44 -0400

    feat: implement Prover interface

---
+++ src/payments/IProver.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.4;
+
+/// @title IProver
+/// @notice Basic interface for prover contracts to: (1) expose proving fees and `Wallet` address, (2) expose function to begin proof validation journey
+interface IProver {
+    /// @notice Gets prover contract's associated `Wallet` address
+    /// @return `Wallet` address to receive proof payment
+    function getWallet() external view returns (address);
+
+    /// @notice Checks if `token` is accepted payment method by prover contract
+    /// @param token token address
+    /// @return `true` if `token` is supported, else `false`
+    function isSupportedToken(address token) external view returns (bool);
+
+    /// @notice Gets proving fee denominated in `token`
+    /// @dev Function `isSupportedToken` is called first
+    /// @return proving fee denominated in `token`
+    function fee(address token) external view returns (uint256);
+
+    /// @notice Request proof validation from prover contract
+    /// @dev Prover contract has to call `validateProof` on coordinator after a proof validation request
+    /// @dev By this point, prover contract has been paid for proof validation
+    function requestProofValidation(uint32 subscriptionId, uint32 interval, address node, bytes calldata proof)
+        external;
+}
